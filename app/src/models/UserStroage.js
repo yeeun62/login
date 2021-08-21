2e007027edfc543435354dfaf76412f07c06a4d0
yeeun62 <byebye62@naver.com>
2021-08-21 14:37:13 +0900

    회원가입 라우팅 구현

--- app/src/models/UserStroage.js
+++ app/src/models/UserStroage.js
@@ -12,7 +12,7 @@ class UserStorage{
         const newUsers = fields.reduce((acc, cur) => {
             if(users.hasOwnProperty(cur)){
                 acc[cur] = users[cur];
-            }
+        }
             return acc;
         }, {});
         return newUsers;
@@ -28,6 +28,14 @@ class UserStorage{
         }, {});
 
         return userInfo;
+    }
+
+    static save(userInfo) {
+        const users = this.#users;
+        users.id.push(userInfo.id);
+        users.name.push(userInfo.name);
+        users.pw.push(userInfo.pw);
+        return {success: true}
     }
 }
 
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,13 @@ const registerBtn = document.querySelector('#button');
 registerBtn.addEventListener('click', register);
 
 function register(){
+    if(!id.value) return alert('아이디를 입력해주세요.')
+    if(pw.value !== confirmPw.value) return alert('비밀번호가 일치하지 않습니다.')
+
     const req = {
         id: id.value,
         name: name.value,
         pw: pw.value,
-        confirmPw: confirmPw.value
     };
 
     fetch("/register", {
