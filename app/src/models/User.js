f1c0913f3bfe55d19975f50c00a006bdc2ad0a57
yeeun62 <byebye62@naver.com>
2021-08-22 16:01:39 +0900

    database

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,15 +9,15 @@ class User {
 
     login() {
         const client = this.body;
-        const {id, pw} = UserStorage.getUserInfo(client.id);
+        UserStorage.getUserInfo(client.id);
 
-        if(id){
-            if (id === client.id && pw === client.pw) {
-                return { success: true};
-            }
-            return {success: false, msg: '비밀번호가 틀렸습니다.'};
-        }
-        return {success: false, msg: '존재하지 않는 아이디입니다.'};
+        // if(id){
+        //     if (id === client.id && pw === client.pw) {
+        //         return { success: true};
+        //     }
+        //     return {success: false, msg: '비밀번호가 틀렸습니다.'};
+        // }
+        // return {success: false, msg: '존재하지 않는 아이디입니다.'};
     }
 
     register() {
--- app/src/models/UserStroage.js
+++ app/src/models/UserStroage.js
@@ -1,14 +1,10 @@
 "use strict";
 
-class UserStorage{
-    static #users = {
-        id: ['yeeun', '김코딩', '박해커'],
-        pw: ['1234', '5678', '0602'],
-        name: ['예은']
-    };
+const fs = require("fs");
 
+class UserStorage{
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((acc, cur) => {
             if(users.hasOwnProperty(cur)){
                 acc[cur] = users[cur];
@@ -19,19 +15,23 @@ class UserStorage{
     }
 
     static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
+        // const users = this.#users;
+        fs.readFile("./package.json", (err, data) => {
+            if(err) throw err;
+            console.log(JSON.parse(data));
+        })
+        // const idx = users.id.indexOf(id);
+        // const usersKeys = Object.keys(users);
+        // const userInfo = usersKeys.reduce((newUser, info) => {
+        //     newUser[info] = users[info][idx];
+        //     return newUser;
+        // }, {});
 
-        return userInfo;
+        // return userInfo;
     }
 
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.pw.push(userInfo.pw);
